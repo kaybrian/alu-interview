1ed559965343a5a3a8b5704a1f8dba98514ecec9
kaybrian <kayonbrian@gmail.com>
2023-05-19 14:35:38 +0200

    add a n abs number

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -9,7 +9,7 @@ def minOperations(n):
         return 0
 
     operations = 0
-    for i in range(2, int(math.sqrt(n)) + 1):
+    for i in range(2, int(math.sqrt(abs(n))) + 1):
         while n % i == 0:
             operations += i
             n //= i
